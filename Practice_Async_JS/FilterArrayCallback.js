b6b52cc1dda808c342c742bcead3e255f3c659f1
Vijeet Shah <93502358+vijeet-shah@users.noreply.github.com>
2023-09-24 14:06:40 +0530

    Update FilterArrayCallback.js

--- Practice_Async_JS/FilterArrayCallback.js
+++ Practice_Async_JS/FilterArrayCallback.js
@@ -12,3 +12,27 @@ let filterValue = (value) => {
 };
 
 let x = [1, 2, 3, 4, 5, 6];
+
+
+/*
+//Write a function 'filterArray' that takes an array and a callback function as arguments. 'filterArray' should filter the elements of the array based on the condition specified by the callback function and return a new array with the filtered elements.
+
+let filterArray = (a, callback) => {
+  let x = a.filter((e) => {
+    return callback(e);
+  });
+  return x;
+};
+
+function moreThanFive(z) {
+  if (z > 5) {
+    return z;
+  }
+}
+
+let arr = [0, 2, 4, 5, 6, 78, 5];
+
+console.log(filterArray(arr, moreThanFive));
+
+
+**/
